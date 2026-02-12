94fc01b47c3d27e610c0cdb322274c5aea8f5ed8
William Saluveer <williamsaluveer@Williams-MacBook-Pro-3.local>
2026-02-12 15:31:09 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import Hero from '../components/Hero'
 import SecuritySection from '../components/SecuritySection'
-import TrustSection from '../components/TrustSection'
 import Testimonials from '../components/Testimonials'
 import HomeCtaSection from '../components/HomeCtaSection'
 import HomeOfferSection from '../components/HomeOfferSection'
@@ -10,7 +9,6 @@ function Home() {
     <main>
       <Hero />
       <SecuritySection />
-      <TrustSection />
       <HomeOfferSection />
       <Testimonials />
       <HomeCtaSection />
